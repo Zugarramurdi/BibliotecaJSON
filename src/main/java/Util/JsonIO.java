29e5d963d753304e45546514ba4744aacb239943
Zugarramurdi <juanma.segura.canales@gmail.com>
2025-11-08 01:14:51 +0100

    Initial commit

---
+++ src/main/java/Util/JsonIO.java
@@ -0,0 +1,19 @@
+package Util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class JsonIO {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+    private JsonIO() {}
+
+    public static
+
+
+}
